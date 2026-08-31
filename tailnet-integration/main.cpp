b915b4b79eab44e68b8de74404436b91ee5db74f
Rusty Conover <rusty@conover.me>
2026-08-31 17:11:26 -0400

    style: format Tailnet HTTP client

--- tailnet-integration/main.cpp
+++ tailnet-integration/main.cpp
@@ -432,9 +432,7 @@ void run_http_client(int argc, char** argv) {
                    "--expect-proxy"});
     const auto spoof_login = options.require("--spoof-login");
     auto builder = HttpClient::builder(options.require("--url"));
-    builder.prefix("")
-        .protocol_version("2.0.0")
-        .header("Tailscale-User-Login", spoof_login);
+    builder.prefix("").protocol_version("2.0.0").header("Tailscale-User-Login", spoof_login);
     auto client = builder.build();
     const auto expected = client_expectation(options, "http");
     const auto request = AnnotatedBatch::data(empty_batch());
